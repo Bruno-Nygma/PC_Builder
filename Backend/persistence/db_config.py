fbd96c24f3abc02356d80ed71c6b41691b04fda1
Bruno Bucci <buccibruno5@gmail.com>
2026-06-09 09:31:53 +0200

    removed init_db()

--- Backend/persistence/db_config.py
+++ Backend/persistence/db_config.py
@@ -8,12 +8,5 @@ engine = create_engine("postgresql://postgres:admin@localhost/PC_builder", echo=
 
 SessionLocal = sessionmaker(bind=engine)
 
-def init_db():
-
-    print("[LOG] - Creazione Database")
-
-    # Base.metadata.drop_all(bind=engine) # Didattico
-    # Base.metadata.create_all(bind=engine)
-
 def get_session():
     return SessionLocal()
